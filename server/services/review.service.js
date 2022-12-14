cbb772db81d77206a0308043da44dead7f5104d9
hasibulislam999 <hasib143sl@gmail.com>
2022-12-14 23:47:21 +0600

    review service init

---
+++ server/services/review.service.js
@@ -0,0 +1,36 @@
+/* internal export */
+const Review = require("../models/Review");
+
+/* insert new review */
+exports.insertNewReview = async (data) => {
+  const result = await Review.create(data);
+  return result;
+};
+
+/* display all reviews */
+exports.displayAllReviews = async () => {
+  const result = await Review.find();
+  return result;
+};
+
+/* display specific review */
+exports.displaySpecificReview = async ({ id }) => {
+  const result = await Review.findById(id);
+  return result;
+};
+
+/* update specific review */
+exports.updateSpecificReview = async (id, data) => {
+  const result = await Review.updateOne(
+    { _id: id },
+    { $set: data },
+    { upsert: true, runValidators: true }
+  );
+  return result;
+};
+
+/* remove specific review */
+exports.removeSpecificReview = async ({ id }) => {
+  const result = await Review.findByIdAndDelete(id);
+  return result;
+};
