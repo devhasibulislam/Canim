453afc7fd63f118f35f9f719b9b4a14984fcebbd
hasibulislam999 <hasib143sl@gmail.com>
2022-12-15 13:28:19 +0600

    home page init

---
+++ client/src/pages/main/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "../../components/Title";
+
+const Home = () => {
+  return (
+    <section>
+      <Title>Home</Title>
+    </section>
+  );
+};
+
+export default Home;
