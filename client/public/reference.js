ed7132f93811266894d226d4d5d8ddd0639c73b6
hasibulislam999 <hasib143sl@gmail.com>
2022-12-15 13:26:37 +0600

    public dir init

---
+++ client/public/reference.js
@@ -0,0 +1,38 @@
+/**
+ * Git refusing to merge unrelated histories on rebase
+ * https://stackoverflow.com/questions/37937984/git-refusing-to-merge-unrelated-histories-on-rebase
+ *
+ * How to Lazy Load Images in React
+ * https://www.freecodecamp.org/news/how-to-lazy-load-images-in-react/
+ *
+ * You are loading Images wrong! Use this instead
+ * https://www.youtube.com/watch?v=8viWcH5bUE4
+ *
+ * React Context Multiple Values Example
+ * https://progressivewebninja.com/how-to-pass-multiple-values-in-react-context/
+ *
+ * How to refresh page with React Router?
+ * https://stackoverflow.com/questions/73218944/how-to-refresh-page-with-react-router
+ *
+ * Store name suggestions
+ * https://namesbee.com/store-names/
+ *
+ * Populate multiple fields
+ * https://stackoverflow.com/questions/12821596/multiple-populates-mongoosejs
+ *
+ * TypeError: split is not a function in JavaScript
+ * https://bobbyhadz.com/blog/javascript-typeerror-string-split-is-not-a-function#:~:text=The%20%22split%20is%20not%20a,the%20split%20method%20on%20strings.
+ *
+ * How to Add to an Array in React State using Hooks
+ * https://javascript.plainenglish.io/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
+ *
+ * Generating random whole numbers in JavaScript in a specific range
+ * https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
+ *
+ * In Mongoose, how do I sort by date? (node.js)
+ * https://stackoverflow.com/questions/5825520/in-mongoose-how-do-i-sort-by-date-node-js
+ *
+ * Error message "error:0308010C:digital envelope routines::unsupported"
+ * https://stackoverflow.com/questions/69692842/error-message-error0308010cdigital-envelope-routinesunsupported
+ * https://www.freecodecamp.org/news/error-error-0308010c-digital-envelope-routines-unsupported-node-error-solved/
+ */
