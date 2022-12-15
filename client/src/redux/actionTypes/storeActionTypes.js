7496052543c6ea56d5e5d58a3893231e01694f55
hasibulislam999 <hasib143sl@gmail.com>
2022-12-15 12:54:55 +0600

    store action types init

---
+++ client/src/redux/actionTypes/storeActionTypes.js
@@ -0,0 +1,9 @@
+const storeActionTypes = {
+  ADD_STORE: "ADD_STORE",
+  REMOVE_STORE: "REMOVE_STORE",
+  DISPLAY_STORE: "DISPLAY_STORE",
+  DISPLAY_STORES: "DISPLAY_STORES",
+  UPDATE_STORE: "UPDATE_STORE",
+};
+
+export default storeActionTypes;
