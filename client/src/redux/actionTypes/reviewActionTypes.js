0be3834e539d1560df3620e7deaf38b30f42c8e7
hasibulislam999 <hasib143sl@gmail.com>
2022-12-15 12:54:49 +0600

    review action types init

---
+++ client/src/redux/actionTypes/reviewActionTypes.js
@@ -0,0 +1,10 @@
+const reviewActionTypes = {
+    ADD_REVIEW: "ADD_REVIEW",
+    REMOVE_REVIEW: "REMOVE_REVIEW",
+    DISPLAY_REVIEW: "DISPLAY_REVIEW",
+    DISPLAY_REVIEWS: "DISPLAY_REVIEWS",
+    UPDATE_REVIEW: "UPDATE_REVIEW",
+  };
+  
+  export default reviewActionTypes;
+  
