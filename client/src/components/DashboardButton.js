ac287caf400ceca23a5d40036ee2fde5a4cf4983
hasibulislam999 <hasib143sl@gmail.com>
2022-12-15 13:25:11 +0600

    dashboard button component init

---
+++ client/src/components/DashboardButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DashboardButton = () => {
+  return (
+    <button
+      type="submit"
+      class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    >
+      Submit
+    </button>
+  );
+};
+
+export default DashboardButton;
