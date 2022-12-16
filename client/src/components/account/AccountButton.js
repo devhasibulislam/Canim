5eca7c676893653a04af94f426ac06f5aadb3201
RKD021 <rayhankhan52cu@gmail.com>
2022-12-16 08:34:46 +0600

    default account button component init

---
+++ client/src/components/account/AccountButton.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const AccountButton = ({ children }) => {
+  return (
+    <button
+      className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none"
+      type="submit"
+    >
+      <svg
+        className="w-6 h-6 -ml-2"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
+        <circle cx="8.5" cy="7" r="4" />
+        <path d="M20 8v6M23 11h-6" />
+      </svg>
+      <span className="ml-3">{children}</span>
+    </button>
+  );
+};
+
+export default AccountButton;
