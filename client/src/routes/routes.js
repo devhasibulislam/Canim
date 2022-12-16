e8b5a2f6d1f5fc4c949410ce637f6457bd06357c
RKD021 <rayhankhan52cu@gmail.com>
2022-12-16 08:30:39 +0600

    account route integrated

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
+import accountRoute from "./accountRoute";
 import dashboardRoute from "./dashboardRoute";
 import mainRoute from "./mainRoute";
 
-const routes = createBrowserRouter([mainRoute, dashboardRoute]);
+const routes = createBrowserRouter([mainRoute, dashboardRoute, accountRoute]);
 
 export default routes;
