ac8db5e20f115fb531b7573809b8a840fa9c5b24
RKD021 <rayhankhan52cu@gmail.com>
2022-12-16 08:32:13 +0600

    integrate local storage token approach to backend to find myself

--- client/src/redux/thunk/user/persistMyAccount.js
+++ client/src/redux/thunk/user/persistMyAccount.js
@@ -2,7 +2,12 @@ import { persistMyAccountData } from "../../actions/userActions";
 
 function persistMyAccount() {
   return async (dispatch, getState) => {
-    const res = await fetch("https://canim.onrender.com/user/myself");
+    const res = await fetch("https://canim.onrender.com/user/myself", {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage?.getItem("accessToken")}`,
+      },
+    });
     const data = await res.json();
 
     if (data.acknowledgement) {
