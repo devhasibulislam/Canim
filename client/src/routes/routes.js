483fa0b81ce438d7d9335c2a30ab09b00e90f488
hasibulislam999 <hasib143sl@gmail.com>
2022-12-17 12:51:56 +0600

    profile route integrate with browser router

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -2,7 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import accountRoute from "./accountRoute";
 import dashboardRoute from "./dashboardRoute";
 import mainRoute from "./mainRoute";
+import profileRoute from "./profileRoute";
 
-const routes = createBrowserRouter([mainRoute, dashboardRoute, accountRoute]);
+const routes = createBrowserRouter([
+  mainRoute,
+  dashboardRoute,
+  accountRoute,
+  profileRoute,
+]);
 
 export default routes;
