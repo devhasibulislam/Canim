2bd11aa3e169d92681ffeeacb41f71e86b6aeff2
hasibulislam999 <hasib143sl@gmail.com>
2022-12-17 23:14:48 +0600

    payment route added to profile dashboard

--- client/src/layout/profile/profileRoutes.js
+++ client/src/layout/profile/profileRoutes.js
@@ -1,5 +1,6 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { SiWish } from "react-icons/si";
+import { MdPayment } from "react-icons/md";
 
 const iconStyle = "lg:h-4 md:h-10 h-8 lg:w-4 md:w-10 w-8 lg:mx-0 md:mx-auto";
 const routes = [
@@ -12,6 +13,11 @@ const routes = [
     icon: <SiWish className={iconStyle} />,
     name: "My Wishlist",
     anchor: "my-wishlist",
+  },
+  {
+    icon: <MdPayment className={iconStyle} />,
+    name: "My Payment",
+    anchor: "my-payment",
   },
 ];
 
