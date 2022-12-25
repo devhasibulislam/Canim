79567ced4c0bc9818928c68e93c4302986d5dbcc
hasibulislam999 <hasib143sl@gmail.com>
2022-12-25 13:03:03 +0600

    home page modify throughout components

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Banner from "../../components/home/Banner";
+import Explore from "../../components/home/Explore";
 import Title from "../../components/Title";
 
 const Home = () => {
   return (
-    <section>
+    <section className="container mx-auto my-4 lg:px-0 px-4">
       <Title>Home</Title>
+      <Banner />
+      <Explore />
     </section>
   );
 };
