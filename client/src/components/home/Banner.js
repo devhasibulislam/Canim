bf9036333d6d5becbcfff0742c5434b11a1cfd14
hasibulislam999 <hasib143sl@gmail.com>
2022-12-25 17:08:28 +0600

    banner component modified

--- client/src/components/home/Banner.js
+++ client/src/components/home/Banner.js
@@ -14,47 +14,51 @@ const Banner = () => {
   }, [dispatch]);
 
   return (
-    <div
-      className="h-56 sm:h-64 xl:h-80 2xl:h-96 bg-repeat bg-contain"
-      style={{ backgroundImage: "url(/slider_bg.svg)" }}
-    >
-      <Carousel>
-        {banners.map(
-          ({
-            title,
-            description,
-            thumbnail: { url, public_id },
-            url: anchor,
-            _id,
-          }) => (
-            <div
-              key={_id}
-              className="flex lg:justify-between md:justify-between lg:flex-row md:flex-row flex-col items-center"
-            >
-              <article className="flex flex-col lg:gap-y-4 md:gap-y-4 gap-y-2 lg:order-1 md:order-1 order-2 w-full">
-                <h1 className="lg:text-6xl md:text-3xl text-xl">{title}</h1>
-                <p className="lg:font-bold md:font-bold text-sm">
-                  {description}
-                </p>
-                <Button
-                  color={"purple"}
-                  className="w-fit"
-                  onClick={() => navigate(`//${anchor}`)}
+    <section className="bg-slate-100">
+      <div className="container mx-auto lg:px-0 px-4 py-4">
+        <div
+          className="h-56 sm:h-64 xl:h-80 2xl:h-96 bg-repeat bg-contain"
+          style={{ backgroundImage: "url(/slider_bg.svg)" }}
+        >
+          <Carousel>
+            {banners.map(
+              ({
+                title,
+                description,
+                thumbnail: { url, public_id },
+                url: anchor,
+                _id,
+              }) => (
+                <div
+                  key={_id}
+                  className="flex lg:justify-between md:justify-between lg:flex-row md:flex-row flex-col items-center"
                 >
-                  Click here
-                </Button>
-              </article>
-              <img
-                src={url}
-                alt={public_id}
-                className="max-w-full lg:w-1/2 md:w-1/2 w-full lg:h-auto md:h-auto h-20 lg:order-2 md:order-2 order-1 object-cover"
-                loading="lazy"
-              />
-            </div>
-          )
-        )}
-      </Carousel>
-    </div>
+                  <article className="flex flex-col lg:gap-y-4 md:gap-y-4 gap-y-2 lg:order-1 md:order-1 order-2 w-full">
+                    <h1 className="lg:text-6xl md:text-3xl text-xl">{title}</h1>
+                    <p className="lg:font-bold md:font-bold text-sm">
+                      {description}
+                    </p>
+                    <Button
+                      color={"purple"}
+                      className="w-fit"
+                      onClick={() => navigate(`//${anchor}`)}
+                    >
+                      Click here
+                    </Button>
+                  </article>
+                  <img
+                    src={url}
+                    alt={public_id}
+                    className="max-w-full lg:w-1/2 md:w-1/2 w-full lg:h-auto md:h-auto h-20 lg:order-2 md:order-2 order-1 object-cover"
+                    loading="lazy"
+                  />
+                </div>
+              )
+            )}
+          </Carousel>
+        </div>
+      </div>
+    </section>
   );
 };
 
