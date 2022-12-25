889fd50a881d43ce54abf2f482b74851fb1ac562
hasibulislam999 <hasib143sl@gmail.com>
2022-12-25 17:09:42 +0600

    home page modify

--- client/src/pages/main/Home.js
+++ client/src/pages/main/Home.js
@@ -1,15 +1,23 @@
 import React from "react";
+import Footer from "../../components/Footer";
 import Banner from "../../components/home/Banner";
+import Brands from "../../components/home/Brands";
+import Categories from "../../components/home/Categories";
 import Explore from "../../components/home/Explore";
+import Stores from "../../components/home/Stores";
 import Title from "../../components/Title";
 
 const Home = () => {
   return (
-    <section className="container mx-auto my-4 lg:px-0 px-4">
+    <>
       <Title>Home</Title>
       <Banner />
       <Explore />
-    </section>
+      <Categories />
+      <Brands />
+      <Stores />
+      <Footer />
+    </>
   );
 };
 
